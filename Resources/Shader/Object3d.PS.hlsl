15b2e3267846e20032b2c28dc17563d8e7e4be4b
k024g0021 <k024g0021@g.neec.ac.jp>
2025-10-03 12:02:47 +0900

    シェーダー調整

--- Resources/Shader/Object3d.PS.hlsl
+++ Resources/Shader/Object3d.PS.hlsl
@@ -35,13 +35,14 @@ PixelShaderOutput main(VertexShaderOutput input)
         float NdotL = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
         // ライトによる色変化
-        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+        output.color.rgb = gMaterial.color.rgb* textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+        output.color.a = gMaterial.color.a * textureColor.a;
       
     }
     else
     {
         // ライティングなしで色をそのまま合成
-        output.color = gMaterial.color * textureColor;
+        output.color= gMaterial.color * textureColor;
     }
     return output;
 }
